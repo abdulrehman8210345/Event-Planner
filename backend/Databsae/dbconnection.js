f63ed996cfacab364e4776c636d9b79e0b99ab80
abdulrehman8210345 <abdulrehman8210345@gmail.com>
2024-05-27 15:50:32 +0500

    more changes

--- backend/Databsae/dbconnection.js
+++ backend/Databsae/dbconnection.js
@@ -1,7 +1,7 @@
 import mongoose from "mongoose";
 
 export const connectiondb = ()=>{
-    mongoose.connect(process.env.MONGO_URL,{dbName:"eventplanning"}).then(()=>{
+    mongoose.connect(process.env.MONGO_URI,{dbName:"eventplanning"}).then(()=>{
         console.log("db connected locally");
     }).catch((err)=>{
         console.log("db not connected",err);
